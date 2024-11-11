66a8bef30c28cd59b884cc2c34ad64e8f114c84d
unique0902 <rla0591@naver.com>
2024-11-12 01:35:33 +0900

    Feat: add todoEdit page

--- src/pages/Todo/Edit/TodoEdit.tsx
+++ src/pages/Todo/Edit/TodoEdit.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DynamicForm from '../../../components/DynamicForm/DynamicForm';
+import { Todo } from '../../../data/type';
 
 interface Props {}
 
 const TodoEdit: React.FC<Props> = () => {
-  return <div>TodoEdit</div>;
+  const [todo, setTodo] = useState<Todo>({
+    id: '',
+    title: '',
+    description: '',
+    date: '',
+    eta: '',
+    difficultyLevel: '',
+    projectId: '',
+    isAccomplished: false,
+  });
+  return (
+    <div>
+      <DynamicForm data={todo} onChange={setTodo} />
+    </div>
+  );
 };
 
 export default TodoEdit;
